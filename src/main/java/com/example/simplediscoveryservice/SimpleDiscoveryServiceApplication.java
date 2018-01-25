4ae9819c6f27306c614a490f7ce4bbdc08a9a6fa
비디 조현구 <hgcho@bluedigm.com>
2018-01-25 09:48:58 +0900

    First commit

---
+++ src/main/java/com/example/simplediscoveryservice/SimpleDiscoveryServiceApplication.java
@@ -0,0 +1,12 @@
+package com.example.simplediscoveryservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleDiscoveryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleDiscoveryServiceApplication.class, args);
+	}
+}
---
+++ src/test/java/com/example/simplediscoveryservice/SimpleDiscoveryServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.simplediscoveryservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SimpleDiscoveryServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}

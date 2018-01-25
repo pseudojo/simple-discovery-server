f4318dc985640e816b240f517aec554320cd7565
비디 조현구 <hgcho@bluedigm.com>
2018-01-25 10:29:36 +0900

    Add logic for Eureka server simply.

--- src/main/java/com/example/simplediscoveryserver/SimpleDiscoveryServerApplication.java
+++ src/main/java/com/example/simplediscoveryserver/SimpleDiscoveryServerApplication.java
@@ -2,8 +2,10 @@ package com.example.simplediscoveryserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer        // included Ribbon client
 public class SimpleDiscoveryServerApplication {
 
 	public static void main(String[] args) {
